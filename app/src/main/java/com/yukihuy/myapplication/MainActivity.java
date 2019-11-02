fe3b78f2913e06eb6cddff65e9157e5a77f0c14e
yukihuy99yuki <36331785+yukihuy99yuki@users.noreply.github.com>
2019-11-02 13:35:36 +0700

    Create project

---
+++ app/src/main/java/com/yukihuy/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.yukihuy.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        Intent intent = new Intent(MainActivity.this,Login.class);
+        startActivity(intent);
+    }
+}
