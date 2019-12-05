ec7b8f47c7dc1e9eebe738d01ce71424fd70bf8a
yukihuy99yuki <36331785+yukihuy99yuki@users.noreply.github.com>
2019-12-05 12:22:13 +0700

    Chỉnh sữa giao diện

--- app/src/main/java/com/yukihuy/myapplication/MainActivity.java
+++ app/src/main/java/com/yukihuy/myapplication/MainActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_linh_vuc);
+
     }
     public void onClick(View view){
         Intent intent = new Intent(MainActivity.this,Login.class);
         startActivity(intent);
+    }
+    public void setLayout(){
+
     }
 }
