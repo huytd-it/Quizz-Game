44823af3f22c1042eefd02f32eea4144f74df001
yukihuy99yuki <36331785+yukihuy99yuki@users.noreply.github.com>
2019-12-11 21:24:10 +0700

    Add Question Activity + API

--- app/src/main/java/com/yukihuy/myapplication/MainActivity.java
+++ app/src/main/java/com/yukihuy/myapplication/MainActivity.java
@@ -4,22 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linh_vuc);
+        setContentView(R.layout.activity_main);
 
     }
     public void onClick(View view){
-        Intent intent = new Intent(MainActivity.this,Login.class);
+        Intent intent = new Intent(MainActivity.this, LinhVucActivity.class);
         startActivity(intent);
     }
+
     public void setLayout(){
 
     }
